0493cd9a291e7bb0b857e52373cb6aa6f8cf5319
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-15 23:19:31 -0300

    Added types auth

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
   eventClearActiveEvents: "[event] Clear New Active Events",
   eventUpdated: "[event] Updated",
   eventDeleted: "[event] Deleted",
+
+  authChecking: "[auth] Checking login state",
+  authCheckingFinish: "[auth] Finish checking login state",
+  authStartLogin: "[auth] Start login",
+  authLogin: "[auth] Login",
+  authStartRegister: "[auth] Start register",
+  authStartTokenRenew: "[auth] Start token renew",
+  authLogout: "[auth] Logout",
 };
