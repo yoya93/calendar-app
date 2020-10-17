f2b17a2dbaebb88a8a90a659b1abbda63597c5d5
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-16 23:32:43 -0300

    Added name in navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Navbar = () => {
+  const { name } = useSelector((state) => state.auth);
   return (
     <div className="navbar navbar-dark bg-dark mb-4">
-      <span className="navbar-brand">Pedro</span>
+      <span className="navbar-brand">{name}</span>
       <button className="btn btn-outline-danger">
         <i className="fas fa-sign-out-alt"></i>
         <span> Salir</span>
